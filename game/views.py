2a9f51690092eaa76ff7d7003be62ec9e0dc0977
yakoub abaya <sting606@gmail.com>
2021-09-17 14:13:51 +0300

    teams mixin

--- game/views.py
+++ game/views.py
@@ -2,45 +2,9 @@ from django.views.generic import DetailView, ListView
 from django.core.paginator import Paginator
 from .models import GameMatch, GamePlayer, PlayerIndex
 
-class GameList(ListView):
-    
-    model = GameMatch
-    paginate_by = 5
+class TeamsMixin:
 
-    def get_queryset(self):
-        queryset = super(GameList, self).get_queryset()
-        return queryset.prefetch_related('gameplayer_set__player').order_by('-id')
-
-    def get_context_data(self, **kwargs):
-        context = super(GameList, self).get_context_data(**kwargs)
-        for game in context['object_list']:
-            game.red_players = []
-            game.blue_players = []
-            game.spectators = []
-            for player in game.gameplayer_set.all():
-                if (player.team == 1):
-                    game.red_players.append(player)
-                elif (player.team ==2):
-                    game.blue_players.append(player)
-                else :
-                    game.spectators.append(player)
-        return context
-
-    def get_template_names(self):
-        return ['game/list.html', 'list.html']
-
-
-class GameView(DetailView):
-
-    model = GameMatch
-    
-    def get_queryset(self):
-        queryset = super(GameView, self).get_queryset()
-        return queryset.prefetch_related('gameplayer_set__player')
-
-    def get_context_data(self, **kwargs):
-        context = super(GameView, self).get_context_data(**kwargs)
-        game = context['gamematch']
+    def teams_context(self, game):
         game.red_players = []
         game.blue_players = []
         game.spectators = []
@@ -52,9 +16,39 @@ class GameView(DetailView):
             else :
                 game.spectators.append(player)
 
+class GameList(TeamsMixin, ListView):
+    
+    model = GameMatch
+    paginate_by = 5
+
+    def get_queryset(self):
+        queryset = super(GameList, self).get_queryset()
+        return queryset.prefetch_related('gameplayer_set__player').order_by('-id')
+
+    def get_context_data(self, **kwargs):
+        context = super(GameList, self).get_context_data(**kwargs)
+        for game in context['object_list']:
+            self.teams_context(game) 
         return context
 
-class PlayerView(DetailView):
+    def get_template_names(self):
+        return ['game/list.html', 'list.html']
+
+
+class GameView(TeamsMixin, DetailView):
+
+    model = GameMatch
+    
+    def get_queryset(self):
+        queryset = super(GameView, self).get_queryset()
+        return queryset.prefetch_related('gameplayer_set__player')
+
+    def get_context_data(self, **kwargs):
+        context = super(GameView, self).get_context_data(**kwargs)
+        self.teams_context(context['gamematch']) 
+        return context
+
+class PlayerView(TeamsMixin, DetailView):
 
     model = PlayerIndex
 
@@ -68,6 +62,8 @@ class PlayerView(DetailView):
         paginator = Paginator(game_list, 10)
         page_number = self.request.GET.get('page')
         context['game_list'] = paginator.get_page(page_number)
+        for game in context['game_list']:
+            self.teams_context(game) 
 
         context['alias_list'] = PlayerIndex.objects\
             .filter(guid = self.object.guid)\
