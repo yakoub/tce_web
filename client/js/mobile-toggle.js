79268099819f60a78c61b3b422d4186bf2b61b81
yakoub abaya <sting606@gmail.com>
2021-09-27 19:36:13 +0300

    add page inheritance level which allows overriding navigation titles

--- client/js/mobile-toggle.js
+++ client/js/mobile-toggle.js
@@ -1,14 +1,11 @@
 document.addEventListener('DOMContentLoaded', function(event) {
-  let main = document.querySelector('#page > main')
-  let sidebar = document.querySelector('#page > aside')
-  document.querySelector('#page nav select').addEventListener('change', function(event) {
-    if (this.value == 'stats') {
-      main.style.display = 'none'
-      sidebar.style.display = 'block'
-    }
-    else {
-      main.style.display = 'block'
-      sidebar.style.display = 'none'
-    }
+  let page = document.querySelector('#page')
+  let navigation = document.querySelector('#page nav select')
+  let current = navigation.value
+
+  navigation.addEventListener('change', function(event) {
+    page.children[current].classList.add('not-shown')
+    current = this.value
+    page.children[current].classList.remove('not-shown')
   });
 });
